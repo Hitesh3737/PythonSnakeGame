8fa0e9d9c9d2ed0138399da78ad1fd968a987d63
hiteshsajnani <155468034+Hitesh3737@users.noreply.github.com>
2024-01-31 14:53:56 +0530

    Second Commit
    Second Date: 31-01-2024

---
+++ Hspeed.py
@@ -0,0 +1,312 @@
+import pygame
+import sys
+import random
+from pygame.math import Vector2
+
+# Font
+def get_font(size):
+    return pygame.font.Font(None, size)
+
+def get_font(size):
+    return pygame.font.Font("Font/PongGame.ttf", size)
+
+class Button:
+    def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+        self.image = image
+        self.rect = self.image.get_rect(center=pos)
+        self.text = font.render(text_input, True, "#d7fcd4")
+        self.base_color = base_color
+        self.hovering_color = hovering_color
+
+    def change_color(self, mouse_pos):
+        if self.rect.collidepoint(mouse_pos):
+            self.image.fill(pygame.Color(self.hovering_color))
+        else:
+            self.image.fill(pygame.Color("#31572c"))
+
+    def update(self, screen):
+        screen.blit(self.image, self.rect.topleft)
+        text_rect = self.text.get_rect(center=self.rect.center)
+        screen.blit(self.text, text_rect.topleft)
+
+    def check_for_input(self, mouse_pos):
+        return self.rect.collidepoint(mouse_pos)
+
+class Snake:
+    def __init__(self):
+        self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
+        self.direction = Vector2(0, 0)
+        self.new_block = False
+
+        self.head_up = pygame.image.load('Graphics/head_up.png').convert_alpha()
+        self.head_down = pygame.image.load('Graphics/head_down.png').convert_alpha()
+        self.head_right = pygame.image.load('Graphics/head_right.png').convert_alpha()
+        self.head_left = pygame.image.load('Graphics/head_left.png').convert_alpha()
+
+        self.tail_up = pygame.image.load('Graphics/tail_up.png').convert_alpha()
+        self.tail_down = pygame.image.load('Graphics/tail_down.png').convert_alpha()
+        self.tail_right = pygame.image.load('Graphics/tail_right.png').convert_alpha()
+        self.tail_left = pygame.image.load('Graphics/tail_left.png').convert_alpha()
+
+        self.body_vertical = pygame.image.load('Graphics/body_vertical.png').convert_alpha()
+        self.body_horizontal = pygame.image.load('Graphics/body_horizontal.png').convert_alpha()
+
+        self.body_tr = pygame.image.load('Graphics/body_tr.png').convert_alpha()
+        self.body_tl = pygame.image.load('Graphics/body_tl.png').convert_alpha()
+        self.body_br = pygame.image.load('Graphics/body_br.png').convert_alpha()
+        self.body_bl = pygame.image.load('Graphics/body_bl.png').convert_alpha()
+        self.crunch_sound = pygame.mixer.Sound('Sound/crunch.wav')
+
+    def draw_snake(self):
+        self.update_head_graphics()
+        self.update_tail_graphics()
+
+        for index, block in enumerate(self.body):
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
+
+            if index == 0:
+                screen.blit(self.head, block_rect)
+            elif index == len(self.body) - 1:
+                screen.blit(self.tail, block_rect)
+            else:
+                previous_block = self.body[index + 1] - block
+                next_block = self.body[index - 1] - block
+                if previous_block.x == next_block.x:
+                    screen.blit(self.body_vertical, block_rect)
+                elif previous_block.y == next_block.y:
+                    screen.blit(self.body_horizontal, block_rect)
+                else:
+                    if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
+                        screen.blit(self.body_tl, block_rect)
+                    elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
+                        screen.blit(self.body_bl, block_rect)
+                    elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
+                        screen.blit(self.body_tr, block_rect)
+                    elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
+                        screen.blit(self.body_br, block_rect)
+
+    def update_head_graphics(self):
+        head_relation = self.body[1] - self.body[0]
+        if head_relation == Vector2(1, 0):
+            self.head = self.head_left
+        elif head_relation == Vector2(-1, 0):
+            self.head = self.head_right
+        elif head_relation == Vector2(0, 1):
+            self.head = self.head_up
+        elif head_relation == Vector2(0, -1):
+            self.head = self.head_down
+
+    def update_tail_graphics(self):
+        tail_relation = self.body[-2] - self.body[-1]
+        if tail_relation == Vector2(1, 0):
+            self.tail = self.tail_left
+        elif tail_relation == Vector2(-1, 0):
+            self.tail = self.tail_right
+        elif tail_relation == Vector2(0, 1):
+            self.tail = self.tail_up
+        elif tail_relation == Vector2(0, -1):
+            self.tail = self.tail_down
+
+    def move_snake(self):
+        if self.new_block:
+            body_copy = self.body[:]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+            self.new_block = False
+        else:
+            body_copy = self.body[:-1]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+
+    def add_block(self):
+        self.new_block = True
+
+    def play_crunch_sound(self):
+        self.crunch_sound.play()
+
+    def reset(self):
+        self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
+        self.direction = Vector2(0, 0)
+
+class Fruit:
+    def __init__(self):
+        self.randomize()
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)
+        screen.blit(apple, fruit_rect)
+
+    def randomize(self):
+        self.pos = Vector2(random.randint(0, cell_number - 1), random.randint(0, cell_number - 1))
+        print('yumm!!!')
+        
+def main_menu():
+    print("Options")
+    from options import main_menu2
+    main_menu2()
+
+class Main:
+    def __init__(self):
+        self.snake = Snake()
+        self.fruit = Fruit()
+        self.in_game = True
+
+    def reset_game(self):
+        # Reset the game state
+        self.snake = Snake()
+        self.fruit = Fruit()
+        self.in_game = True
+
+    def run(self):
+        while self.in_game:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.K_BACKSPACE:
+                    self.reset_game()
+
+            pygame.event.pump()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                # Handle other events
+
+            self.update()
+            self.draw_elements()
+            pygame.display.flip()
+
+            # Control the frame rate
+            pygame.time.Clock().tick(600)
+            
+
+    def update(self):
+        self.snake.move_snake()
+        self.check_collision()
+        self.check_fail()
+
+    def draw_elements(self):
+        if self.in_game:
+            self.draw_grass()
+            self.fruit.draw_fruit()
+            self.snake.draw_snake()
+            self.draw_score()
+        else:
+            main_menu()
+
+    def check_collision(self):
+        if self.fruit.pos == self.snake.body[0]:
+            self.fruit.randomize()
+            self.snake.add_block()
+            self.snake.play_crunch_sound()
+
+        for block in self.snake.body[1:]:
+            if block == self.fruit.pos:
+                self.fruit.randomize()
+
+    def check_fail(self):
+        if not 0 <= self.snake.body[0].x < cell_number or not 0 <= self.snake.body[0].y < cell_number:
+            self.game_over()
+
+        for block in self.snake.body[1:]:
+            if block == self.snake.body[0]:
+                self.game_over()
+
+    def game_over(self):
+        self.snake.reset()
+        self.in_game = True
+        
+    def draw_grass(self):
+        grass_color = (167, 209, 61)
+        for row in range(cell_number):
+            if row % 2 == 0:
+                for col in range(cell_number):
+                    if col % 2 == 0:
+                        grass_rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
+            else:
+                for col in range(cell_number):
+                    if col % 2 != 0:
+                        grass_rect = pygame.Rect(col * cell_size, row * cell_size, cell_size, cell_size)
+                        pygame.draw.rect(screen, grass_color, grass_rect)
+
+    def draw_score(self):
+        score_text = str(len(self.snake.body) - 3)
+        score_surface = game_font.render(score_text, True, (56, 74, 12))
+        score_x = int(cell_size * cell_number - 60)
+        score_y = int(cell_size * cell_number - 40)
+        score_rect = score_surface.get_rect(center=(score_x, score_y))
+        apple_rect = apple.get_rect(midright=(score_rect.left, score_rect.centery))
+        bg_rect = pygame.Rect(apple_rect.left, apple_rect.top, apple_rect.width + score_rect.width + 6,
+                              apple_rect.height)
+
+        pygame.draw.rect(screen, (167, 209, 61), bg_rect)
+        screen.blit(score_surface, score_rect)
+        screen.blit(apple, apple_rect)
+        pygame.draw.rect(screen, (56, 74, 12), bg_rect, 2)
+
+pygame.mixer.pre_init(44100, -16, 2, 512)
+pygame.init()
+menu_mouse_pos = pygame.mouse.get_pos()
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((800, 838))
+clock = pygame.time.Clock()
+apple = pygame.image.load('Graphics/apple.png').convert_alpha()
+game_font = pygame.font.Font('Font/PoetsenOne-Regular.ttf', 25)
+
+menu_button = Button(image=pygame.image.load("assets/BTM Rect.png"), pos=(400, 820),
+                     text_input="BACK TO OPTIONS PAGE", font=get_font(25), base_color="#dda15e",
+                     hovering_color=(167, 209, 61))
+
+for button in [menu_button]:
+            button.change_color(menu_mouse_pos)
+            button.update(screen)
+
+SCREEN_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(SCREEN_UPDATE, 75)
+
+main_game = Main()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == SCREEN_UPDATE:
+            main_game.update()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                if main_game.snake.direction.y != 1:
+                    main_game.snake.direction = Vector2(0, -1)
+            if event.key == pygame.K_RIGHT:
+                if main_game.snake.direction.x != -1:
+                    main_game.snake.direction = Vector2(1, 0)
+            if event.key == pygame.K_DOWN:
+                if main_game.snake.direction.y != -1:
+                    main_game.snake.direction = Vector2(0, 1)
+            if event.key == pygame.K_LEFT:
+                if main_game.snake.direction.x != 1:
+                    main_game.snake.direction = Vector2(-1, 0)
+            elif event.key == pygame.K_ESCAPE:
+                main_game.in_game = not main_game.in_game
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            menu_mouse_pos = pygame.mouse.get_pos()
+            if menu_button.check_for_input(menu_mouse_pos):
+                pygame.display.set_caption("Options")
+                print("Options")
+                from options import main_menu2
+                main_menu2()
+
+    screen.fill((175, 215, 70))
+    main_game.draw_elements()
+    
+    menu_mouse_pos = pygame.mouse.get_pos()
+    menu_button.change_color(menu_mouse_pos)
+    menu_button.update(screen)
+    
+    pygame.display.update()
+    clock.tick(60)
---
+++ options.py
@@ -0,0 +1,201 @@
+import numpy as np
+import pygame
+import sys
+import imageio
+
+
+# Set up the screen
+width, height = 1280, 720
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Options")
+
+# Load background GIF
+background_gif_path = "assets/backgroundgif.gif"
+reader = imageio.get_reader(background_gif_path)
+
+# Colors
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+# Font
+def get_font(size):
+    return pygame.font.Font(None, size)
+
+# Text content
+menu_text_content = "OPTIONS"
+
+# Border properties
+border_thickness = 4
+border_color = black
+
+clock = pygame.time.Clock()
+
+class Button:
+    def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+        self.image = image
+        self.rect = self.image.get_rect(center=pos)
+        self.text = font.render(text_input, True, "#d7fcd4")
+        self.base_color = base_color
+        self.hovering_color = hovering_color
+
+    def change_color(self, mouse_pos):
+        if self.rect.collidepoint(mouse_pos):
+            self.image.fill(pygame.Color(self.hovering_color))
+        else:
+            self.image.fill(pygame.Color("#31021f"))
+
+    def update(self, screen):
+        screen.blit(self.image, self.rect.topleft)
+        text_rect = self.text.get_rect(center=self.rect.center)
+        screen.blit(self.text, text_rect.topleft)
+
+    def check_for_input(self, mouse_pos):
+        return self.rect.collidepoint(mouse_pos)
+
+def play_sound(sound_name):
+    if sound_name == 'ding':
+        sound = pygame.mixer.Sound("resources/ding.mp3")
+        pygame.mixer.Sound.play(sound)
+
+def get_font(size):
+    return pygame.font.Font("assets/font.ttf", size)
+
+def Highspeed():
+    play_sound("ding")
+    pygame.display.set_caption("HARD MODE")
+    
+    print("Creating an instance of the Main class...")
+    from Hspeed import Main
+    game = Main()  # Create an instance of the Main class
+    while game.in_game:
+          game.run()
+
+    print("Game finished. Creating play_again_button...")
+
+    play_again_button = Button(image=None, pos=(640, 460),
+                               text_input="PLAY AGAIN", font=get_font(75), base_color="black", hovering_color="green")
+
+    play_again_button.change_color(pygame.mouse.get_pos())
+    play_again_button.update(screen)
+
+    pygame.display.update()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.K_BACKSPACE:
+            if play_again_button.check_for_input(pygame.mouse.get_pos()):
+                # Reset the game state before returning to the menu
+                game.reset_game()
+                return
+
+def ensure_valid_array(frame):
+    # Ensure 3D structure (height, width, depth)
+    if frame.ndim == 2:
+        frame = np.expand_dims(frame, axis=2)
+    elif frame.ndim == 1:
+        # Handle 1D array (grayscale)
+        frame = np.expand_dims(frame, axis=0)
+        frame = np.expand_dims(frame, axis=0)
+    return frame
+
+def Infinity():
+         pygame.display.set_caption("EASY MODE")
+         print("EASY MODE")
+         from infinity import main_game
+         main_game()
+
+pygame.display.update()
+
+def Basic():
+         pygame.display.set_caption("BASIC SNAKE GAME")
+         print("BASIC SNAKE GAME")
+         from alter import SnakeGame
+         SnakeGame()
+
+pygame.display.update()
+
+def main_menu2():
+    frame_index = 0
+
+    while True:
+        # Clear the screen
+        screen.fill((255, 255, 255))
+        
+        try:
+            # Blit the current frame onto the screen
+            frame = reader.get_data(frame_index)
+            frame = ensure_valid_array(frame)
+
+            # Ensure the frame is a valid 3D array
+            if frame.ndim == 3 and frame.shape[2] == 3:
+                frame_surface = pygame.surfarray.make_surface(frame)
+
+                # Get the original dimensions of the frame
+                original_width, original_height, _ = frame.shape
+
+                # Calculate the scaling factors to maintain the original aspect ratio
+                aspect_ratio = original_width / original_height
+                new_height = int(1400 / aspect_ratio)  # Calculate new height based on the desired width (720)
+                frame_surface = pygame.transform.scale(frame_surface, (1280, new_height))
+
+                screen.blit(frame_surface, (0, 0))
+            else:
+                print("Invalid frame format.")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+        # Update the frame index
+        frame_index = (frame_index + 1) % len(reader)
+
+        menu_mouse_pos = pygame.mouse.get_pos()
+        
+        # Render the text with a border
+        menu_text_surface = get_font(100).render(menu_text_content, True, border_color)
+        menu_text_rect = menu_text_surface.get_rect(center=(width // 2 + border_thickness, 100 + border_thickness))
+        screen.blit(menu_text_surface, menu_text_rect)
+
+        # Render the actual text on top of the border
+        menu_text_surface = get_font(100).render(menu_text_content, True, "#454B1B")
+        menu_text_rect = menu_text_surface.get_rect(center=(width // 2, 100))
+        screen.blit(menu_text_surface, menu_text_rect)
+        
+        EASYMODE_button = Button(image=pygame.image.load("assets/options Rect.png"), pos=(640, 250),
+                             text_input="EASY MODE", font=get_font(55), base_color="#606c38", hovering_color="#454B1B")
+        HARDMODE_button = Button(image=pygame.image.load("assets/options Rect.png"), pos=(640, 380),
+                                text_input="HARD MODE", font=get_font(55), base_color="#606c38", hovering_color="#454B1B")
+        BASICGAME_button = Button(image=pygame.image.load("assets/BASICSNAKE React.png"), pos=(640, 510),
+                             text_input="BASIC SNAKE GAME", font=get_font(47), base_color="#606c38", hovering_color="#454B1B")
+        menu_button = Button(image=pygame.image.load("assets/MENU React.png"), pos=(640, 630),
+                             text_input="BACK TO MAIN MENU", font=get_font(50), base_color="#606c38", hovering_color="#454B1B")
+        
+
+        for button in [EASYMODE_button, HARDMODE_button, BASICGAME_button, menu_button]:
+            button.change_color(menu_mouse_pos)
+            button.update(screen)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if EASYMODE_button.check_for_input(menu_mouse_pos):
+                    Infinity()
+                if HARDMODE_button.check_for_input(menu_mouse_pos):
+                    Highspeed()
+                if BASICGAME_button.check_for_input(menu_mouse_pos):
+                    Basic()
+                if menu_button.check_for_input(menu_mouse_pos):
+                    play_sound("ding")
+                    pygame.display.set_caption("Menu")
+                    print("Main Menu")
+                    from main import main_menu
+                    main_menu()
+
+        pygame.display.update()
+
+if __name__ == "__main__":
+    pygame.init()
+    pygame.mixer.init()
+    main_menu2()
